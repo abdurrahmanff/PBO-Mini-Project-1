881eb802204311c94f6e590b23e08e690f9db657
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2020-12-20 16:43:56 +0700

    Init Collision Detection with Mouse

---
+++ Collision Detection with Mouse/CollisionDetection/src/id/ac/its/aff231yz160zlp118/collisiondetection/CollisionEx.java
@@ -0,0 +1,27 @@
+package id.ac.its.aff231yz160zlp118.collisiondetection;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class CollisionEx extends JFrame {
+	public CollisionEx() {
+		initUI();
+	}
+	
+	private void initUI() {
+		add (new Board());
+		setResizable(false);
+		pack();
+		
+		setTitle("Collision");
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public static void main (String[] args) {
+		EventQueue.invokeLater(() -> {
+			CollisionEx ex = new CollisionEx();
+			ex.setVisible(true);
+		});
+	}
+}
---
+++ Collision Detection with Mouse/CollisionDetection/src/id/ac/its/aff231yz160zlp118/collisiondetection/SpaceShip.java
@@ -0,0 +1,90 @@
+package id.ac.its.aff231yz160zlp118.collisiondetection;
+
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpaceShip extends Sprite {
+	private int dx;
+	private int dy;
+	private List<Missile> missiles;
+	
+	public SpaceShip(int x, int y) {
+		super(x, y);
+		
+		initCraft();
+	}
+	
+	private void initCraft() {
+		missiles = new ArrayList<>();
+		loadImage("src/resources/spaceship.png");
+		getImageDimensions();
+	}
+
+	public List<Missile> getMissiles() {
+		return missiles;
+	}
+	
+	public void move() {
+        x += dx;
+        y += dy;
+
+        if (x < 1) {
+            x = 1;
+        }
+
+        if (y < 1) {
+            y = 1;
+        }
+    }
+	
+	public void keyPressed(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_SPACE) {
+            fire();
+        }
+
+        if (key == KeyEvent.VK_LEFT) {
+            dx = -3;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 3;
+        }
+
+        if (key == KeyEvent.VK_UP) {
+            dy = -3;
+        }
+
+        if (key == KeyEvent.VK_DOWN) {
+            dy = 3;
+        }
+    }
+
+    public void fire() {
+        missiles.add(new Missile(x + width, y + height / 2));
+    }
+
+    public void keyReleased(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_UP) {
+            dy = 0;
+        }
+
+        if (key == KeyEvent.VK_DOWN) {
+            dy = 0;
+        }
+    }
+}
